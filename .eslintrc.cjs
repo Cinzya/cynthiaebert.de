04eead5a9065aa1d7aba37d66067b38bbb177135
Cinzya <cynti-ebert@web.de>
2022-07-22 06:02:56 +0200

    ADD :: desktop navigation

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    "vue/require-default-prop": "off",
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    "vue/multi-word-component-names": "off",
+    "vue/require-default-prop": "off",
+  },
+};
---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="container flex justify-between align-center">
+    <span class="logo fs-400">Cynthia Ebert</span>
+    <nav class="fs-300">
+      <a href="#">About</a>
+      <a href="#">Experience</a>
+      <a href="#">Work</a>
+      <a href="#">Contact</a>
+    </nav>
+
+    <div class="button">
+      <a href="#">EN</a>
+    </div>
+  </header>
+</template>
