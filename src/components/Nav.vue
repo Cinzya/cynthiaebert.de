1020ead63b2c976004de91935369508582cc116e
Cinzya <cynti-ebert@web.de>
2022-07-23 04:22:22 +0200

    ADD :: mobile nav styling & animation

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const menuState = ref(false);
+const menuVisibility = ref();
+function toggleMenu() {
+  menuState.value = !menuState.value;
+  menuVisibility.value = menuState.value;
+}
+</script>
 
 <template>
-  <header class="container flex justify-between align-center">
+  <header class="container flex justify-between">
     <span class="logo fs-400">Cynthia Ebert</span>
-    <nav class="fs-300">
+
+    <button
+      class="mobile-nav-toggle"
+      :aria-expanded="menuState"
+      artia-controls="primary-navigation"
+    >
+      <img
+        v-if="!menuState"
+        @click="toggleMenu"
+        class="icon-menu"
+        src="../assets/menu.svg"
+        alt=""
+        aria-hidden="true"
+      />
+      <img
+        v-else
+        @click="toggleMenu"
+        class="icon-close"
+        src="../assets/close.svg"
+        alt=""
+        aria-hidden="true"
+      />
+      <span class="visually-hidden">Menu</span>
+    </button>
+    <nav class="fs-300" id="primary-navigation" :data-visible="menuVisibility">
       <a href="#">About</a>
       <a href="#">Experience</a>
       <a href="#">Work</a>
