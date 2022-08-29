32adc82680533666d395e44bab41ecbdbb8e8484
Cinzya <cynti-ebert@web.de>
2022-08-29 02:55:35 +0200

    UPDATE :: layout project section with css grid

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts"></script>
 <template>
-  <article class="project col-start-3 col-end-9 relative">
+  <article class="project col-start-3 col-end-9 inner-grid">
     <img
       src="../assets/img/dummy_projectherodummy_projec.jpg"
       alt=""
-      class="col-7 border-rounded mb-500"
+      class="col-start-1 col-end-8 row-start-1 row-end-4 border-rounded mb-500 row-start-1 row-end-4"
     />
-    <div class="infobox col-4 flex flex-column align-end absolute">
-      <h3 class="fs-600 ff-bold text-shadow mb-500">Project Title</h3>
-      <div class="textbox border-rounded">
-        <p>Example Text</p>
-      </div>
-    </div>
-    <div class="technologies fs-300">
+    <div
+      class="technologies fs-400 col-start-1 col-end-5 row-start-4 row-end-5"
+    >
       <span>Tech1</span>
       <span>Tech2</span>
       <span>Tech4</span>
       <span>Tech5</span>
       <span>Tech6</span>
+    </div>
+    <h3
+      class="fs-600 ff-bold text-shadow mb-500 col-start-5 col-end-9 row-start-2 row-end-3"
+    >
+      Project Title
+    </h3>
+    <div
+      class="textbox border-rounded fs-500 col-start-5 col-end-9 row-start-3 row-end-6"
+    >
+      <p>
+        Et sint excepteur exercitation incididunt non quis ipsum mollit
+        cupidatat velit ea ad proident commodo. Reprehenderit aliquip Lorem
+        exercitation ut occaecat adipisicing do laboris duis. Eiusmod voluptate
+        exercitation magna aliquip cupidatat ex dolor aliqua eu. Nostrud
+        incididunt fugiat est irure.
+      </p>
     </div>
   </article>
 </template>
